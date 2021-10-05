aa1b0ff0d090ec482f72d4b9610174c54e6ff07c
GSantiagoH <giovanni.haysel@gmail.com>
2021-10-05 00:04:34 -0400

    Finished the website theme editing

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 console.log('Its working');
 
+let theme = localStorage.getItem('theme')
+
+if(theme == null){
+    setTheme('light')
+}else{
+    setTheme(theme)
+}
+
 let themeDots = document.getElementsByClassName('theme-dot');
 
 for (var i = 0; themeDots.length > i; i++){
@@ -10,7 +18,7 @@ for (var i = 0; themeDots.length > i; i++){
     })
 }
 
-function setTheme(){
+function setTheme(mode){
     if(mode === 'light'){
         document.getElementById('theme-style').href = "default.css"
     }
@@ -26,4 +34,6 @@ function setTheme(){
     if(mode === 'purple'){
         document.getElementById('theme-style').href = "purple.css"
     }
+
+    localStorage.setItem('theme', mode)
 }
